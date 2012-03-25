95af411faaa057c7a8c640a0d1d89e12e1227d03
Aaron Marks <aaron.marks5@gmail.com>
2012-03-25 02:06:39 -0400

    loading 10 posts, beginning to assemble js array

--- dashboard.php
+++ dashboard.php
@@ -2,13 +2,24 @@
 require_once('tumblroauth/tumblroauth/tumblroauth.php');
 require_once('tumblroauth/config.php');
 include 'utils.php';
-load_posts();
+$post_xml = load_post_xml();
+$posts = post_array_from_xml($post_xml);
 ?>
 <html>
 <head>
     <title>Dashboard</title>
     <link rel='stylesheet' type='text/css' href='styles/dashboard.css' />
     <script src='dashboard.js'></script>
+    <script>
+        var posts = new Array();
+        var i = 0;
+        <?php
+        foreach($post_xml->posts->post as $post) { ?> 
+            posts[i] = "<?php echo $post->attributes()->type; ?>"; 
+
+            i = i+1;
+        <?php } ?>
+    </script>
 </head>
 <div id='type_selectors'>
 <ul>
--- utils.php
+++ utils.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 function query($query) {
     $username = tumblr;
     $password = hackny2012isgreat;
@@ -12,12 +13,28 @@ function query($query) {
     return $row;
 }
 
-function load_posts(){
+function load_post_xml(){
+    $access_token = $_SESSION['access_token']['oauth_token'];
+    $access_secret = $_SESSION['access_token']['oauth_token_secret'];
     $connection = new TumblrOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token, $access_secret);
-    //hit tumblr 1 or 2 times (20 or 40 posts), depending on how long it takes
-    //create js array posts with each having type, innerhtml
+
+    $dash = $connection->dashboard(null);
+    $xml = simplexml_load_string($dash);  //simplexml w/ 10 dash posts
+   
+    return $xml;
 }
 
-function get_access_tokens(){
+function post_array_from_xml($post_xml) {
+    $i = 0;
+    foreach($post_xml->posts->post as $post) {
+        $type = $post->attributes()->type;
+        $posts[$i] = $type;
+
+        if($type == "photo") {
+            print_r($post->{'photo-url'});
+        }
+        $i++;
+    }
 }
+
 ?>
